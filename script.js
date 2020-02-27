67d297d8ead94a04ca4d170716b55317c2d8229a
davide cannerozzi <davide851_@hotmail.com>
2020-02-27 23:45:40 +0100

    add arrow top

---
+++ script.js
@@ -0,0 +1,12 @@
+/* Arrow Scroll On The Top */
+const myButton = document.getElementById('backToTheTop')
+
+window.onscroll = function() {scrollToTheTop()};
+
+scrollToTheTop = () => {
+    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900){
+        myButton.style.display = 'block'
+    }else{
+        myButton.style.display = 'none'
+    }
+}
